a0c7ed234e02829c3a811383f16b9002227049c0
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-01-11 16:35:36 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n <= 0) {
+        n = Math.abs(n);
+    }
+    let result = "";
+    let str = n.toString();
+    for (let i = 0; i < str.length; i++) {
+        result = `${str[i]}${result}`;
+    }
+    return +result;
+};

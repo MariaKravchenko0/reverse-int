161eee7e1834673c60c29e3ed66adda742c3b35f
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-02-21 14:04:11 +0300

    refactor: rewrite solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 module.exports = function reverse(n) {
-    if (n <= 0) {
-        n = Math.abs(n);
-    }
-    let result = "";
-    let str = n.toString();
-    for (let i = 0; i < str.length; i++) {
-        result = `${str[i]}${result}`;
-    }
-    return +result;
+    if (n < 0) n = Math.abs(n);
+    return n.toString().split("").reverse().join("");
 };
